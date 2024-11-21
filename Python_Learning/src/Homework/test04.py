9aa79936e131c81628f1c8cfa9b9ca3a6a6cef29
Learntochangeyoursel <pengsijiaiao@163.com>
2024-11-21 21:30:38 +0800

    添加今天LeetCode的题解

--- Python_Learning/src/Homework/test04.py
+++ Python_Learning/src/Homework/test04.py
@@ -3,6 +3,7 @@
 # 首先需要判断得到的两数是否为整数
 # 其次需要得到两数所有的质因数
 # 然后将最小公质数相乘为最小公倍数，将两数相乘除以最小公倍数则得到最大公约数
+# 如何将两个数作为数组返回
 
 '''解决思路'''
 from src.Homework import test03
@@ -10,25 +11,30 @@ from src.Homework import test03
 
 # 1. 将参数设置为整形
 # 2. 判断两数是否为素数,若为素数，
-# 3.
 
-def judgeTwoNumbers(num1:int ,num2 : int ):
+def judgeTwoNumbers(num1:int ,num2 : int,*args ):
     bool1= test03.judgePrimeNumber02(num1)
     bool2= test03.judgePrimeNumber02(num2)
     if  bool1 and bool2 == True :
         min = 1
         max = num1 * num2
-        print(min,max)
+        args=min,max
     else:
         for i in range(2,num1+1):
-            print(i)
             if num2 % i == 0 and num1 % i == 0 :
                 min = i
                 max = int(num1 * num2 / i)
-                print(min,max)
+                args=min,max
                 break
-    return min,max
+            else :
+                min = 1
+                max = num2 * num1
+                args=min,max
+                break
+    return args
 
-print(judgeTwoNumbers(3,9))
+
+if __name__ == '__main__':
+    print(judgeTwoNumbers(3,4))
 
 
--- Python_Learning/src/Homework/test05.py
+++ Python_Learning/src/Homework/test05.py
@@ -1,1 +1,11 @@
 
+def is_palindrome(num):
+    # 将数字转换为字符串
+    str_num = str(num)
+    # 检查字符串与其反转是否相同
+    return str_num == str_num[::-1]
+
+# 测试函数
+numbers = [121, -121, 10, 12321]
+for number in numbers:
+    print(f"{number} is a palindrome: {is_palindrome(number)}")
